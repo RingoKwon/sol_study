dbf97141795f434e65fe96335f8c8d6ea8758d4b
RingoKwon <ringo.kwon@gmail.com>
2024-11-19 20:43:01 +0900

    update

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,7 +2,10 @@
 
 pragma solidity 0.8.15;
 
-contract MyContract{
+contract MyContract {
+    string public ourString = "hello world ff";
 
-    string public ourString = "hello world ff"; 
+    function updateOutSring(string memory _updateString) public {
+        ourString = _updateString;
+    }
 }
